176abca4b9571b3e148b1f24b736b9bedc208003
jalonso83 <jl.alonso@ce.pucmm.edu.do>
2023-07-03 15:41:13 -0400

    creando atributos de paciente

--- ProyectoFinalDOO/src/logico/Paciente.java
+++ ProyectoFinalDOO/src/logico/Paciente.java
@@ -1,6 +1,16 @@
 package logico;
 
 public class Paciente {
+	
+	public String getMonbre() {
+		return monbre;
+	}
+
+	public void setMonbre(String monbre) {
+		this.monbre = monbre;
+	}
+
+	private String monbre;
 
 	public Paciente() {
 		// TODO Auto-generated constructor stub
